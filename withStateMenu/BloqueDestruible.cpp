1bdf8f240c97a793767c7f107b1424068b70c67a
rsky <131737340+kimyork32@users.noreply.github.com>
2024-07-26 12:10:27 -0500

    Delete withStateMenu directory

--- withStateMenu/BloqueDestruible.cpp
+++
@@ -1,7 +0,0 @@
-#include "BloqueDestruible.h"
-
-BloqueDestruible::BloqueDestruible(float x, float y) : Bloque(x, y) {
-	hitBox.setFillColor(sf::Color::Magenta);
-	this->type = "BD";
-	this->vidas = 1;
-}
